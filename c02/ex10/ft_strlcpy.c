57e70ff78bb674ff380eff6a75e1fdaf64dcf0f0
fuslu725 <us.fatma.lu7@gmail.com>
2024-09-01 12:12:24 +0300

    Update ft_strlcpy.c

--- c02/ex10/ft_strlcpy.c
+++ c02/ex10/ft_strlcpy.c
@@ -24,15 +24,6 @@ unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
     }
 
     i = 0;
-   
-    if (size == 0) 
-    {
-        
-        dest[i] = '\0';
-        return len;
-    }
-
-    i = 0;
     while (src[i] != '\0' && i < size - 1)
     {
        dest[i] = src[i];
