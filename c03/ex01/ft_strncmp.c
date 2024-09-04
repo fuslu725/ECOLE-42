998a82b475f04e883834c210532ba40225df187d
fuslu725 <us.fatma.lu7@gmail.com>
2024-09-04 11:06:21 +0300

    Update ft_strncmp.c

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -18,9 +18,9 @@ int ft_strncmp(char *s1, char *s2, unsigned int n)
     while((s1[i] || s2[i]) && (i < n ))
     {
         if (s1[i] < s2[i])
-			return (-1);
-		else if (s1[i] > s2[i])
-			return (1);
+	  return (-1);
+	else if (s1[i] > s2[i])
+	  return (1);
 		i++;
 
     }
